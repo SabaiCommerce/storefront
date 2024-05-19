e1b9df0a4cd3fc74cd6c93a9b3126494261ad872
Liratanak <annliratanak@gmail.com>
2024-05-19 18:53:21 +0700

    change home page, product page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+    <main className="flex flex-col justify-between">
+      Home
     </main>
   );
 }
